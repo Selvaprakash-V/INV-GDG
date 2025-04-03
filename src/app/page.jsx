74664082d368efb604fa4fbf06bfd0a2672e22d8
Sasitharan06 <skgraphicmadurai33@gmail.com>
2025-04-03 22:51:35 +0530

    s

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,22 @@
 // app/page.tsx
 import { redirect } from 'next/navigation'
+import Link from 'next/link';
 
 export default function Home() {
-  redirect('/dashboard')
+  redirect('/home');
+  
+  // This return statement won't actually execute because of the redirect above,
+  // but it needs to be valid JSX if you remove the redirect
+  return (
+    <div>
+      <Link href="/about">About</Link>
+      <Link href="/role">Role</Link>
+      <Link href="/home">Home</Link>
+      <Link href="/dashboard">Dashboard</Link>
+      <Link href="/exploreseller">Explore Seller</Link>
+      <Link href="/explorecustomer">Explore Customer</Link>
+      <Link href="/signup">Sign Up</Link>
+      <Link href="/login">Login</Link>
+    </div>
+  );
 }
